186331941c210e732a6bff74db6a1edb2af87be3
PraneshSrinivasan <psrinivasan@fleetstudio.com>
2021-10-13 01:21:01 +0530

    diff done

--- src/Get/GetCommits.js
+++ src/Get/GetCommits.js
@@ -3,20 +3,20 @@ import { useParams } from 'react-router';
 
 function GetCommits(){
 
-    const {owner,repository,oid}=useParams();
-    const curl=`https://api.github.com/repos/${owner}/${repository}/commits/${oid}`;
-    const [commits, setGetCommits] = useState(null)
+    const {owner,repository,oid} = useParams();
+    const url=`https://api.github.com/repos/${owner}/${repository}/commits/${oid}`;
+    const [commits, setGetCommits] = useState([])
 
     useEffect( () => {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', curl, true);
+        xhr.open('GET', url, true);
 
         xhr.onload = function () {
             setGetCommits(this.response)
         }
         xhr.send()
 
-    },[curl])
+    },[url])
 
     return(
         <pre> {commits} </pre>
--- src/Get/GetDiff.js
+++ src/Get/GetDiff.js
@@ -3,31 +3,30 @@ import { useParams } from 'react-router';
 
 function GetDiff(){
 
-    const {owner,repository,oid}=useParams
+    const {owner,repository,oid}=useParams();
     const curl=`https://api.github.com/repos/${owner}/${repository}/commits/${oid}`;
-    const [diff, setGetDiff] = useState(null)
-
+    const [diff, setGetDiff] = useState([]);
     useEffect( () => {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', curl, true);
-        xhr.onload = function () {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', curl, true);
 
-            const data = JSON.parse(this.response);
-            var psha = data.parents[0].sha;//parent sha
+            xhr.onload = function () {
+                const data = JSON.parse(this.response);
+                var psha = data.parents[0].sha; //parent sha
+                //const durl=`https://api.github.com/repos/${owner}/${repository}/commits/${oid}`;
+                const durl = `https://github.com/${owner}/${repository}/compare/${psha}...${oid}.diff`;
 
-            const durl=`https://github.com/${owner}/${repository}/compare/${psha}...${oid}.diff`;
-            var dxhr = new XMLHttpRequest();
+                var dxhr = new XMLHttpRequest();
+                dxhr.open('GET', durl, true);
 
-            dxhr.open('GET', durl, true);
-            dxhr.onload = function () {
-                setGetDiff(this.response)
-            }
-            dxhr.send();   
-        }
-        xhr.send()
-    },[curl])
-
-
+                dxhr.onload = function () {
+                    setGetDiff(this.response);
+                };
+                dxhr.send();
+            };
+            xhr.send();
+        },[curl])
+    
     return(
         <pre> {diff} </pre>
     )
